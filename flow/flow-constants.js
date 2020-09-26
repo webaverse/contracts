8aa4410fd573b9f06341ef00ad755a268a25e4f1
Avaer Kazmer <a@modules.io>
2020-09-26 01:02:21 -0400

    Add flow constants

---
+++ flow/flow-constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: 'https://access-testnet.onflow.org',
+
+  FungibleToken: '0x9a0766d93b6608b7',
+  FlowToken: '0x7e60df042a9c0868',
+  NonFungibleToken: '0x631e88ae7f1d7c20',
+
+  ExampleToken: '0xbd726a7affc6afbf',
+  ExampleNFT: '0xa00c3dd0aa47dbf7',
+  ExampleAccount: '0x4ad4afd89179f79b',
+};
