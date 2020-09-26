14bef79279e6e637ac14d8e0acc5525fa6e9ff7b
Avaer Kazmer <a@modules.io>
2020-09-26 08:45:53 -0400

    Update nft address

--- flow/flow-constants.js
+++ flow/flow-constants.js
@@ -6,6 +6,6 @@ export default ({
   NonFungibleToken: '0x631e88ae7f1d7c20',
 
   ExampleToken: '0xbd726a7affc6afbf',
-  ExampleNFT: '0xf7e5fd833902d570',
+  ExampleNFT: '0x0efc13f47d79a1e7',
   ExampleAccount: '0x4ad4afd89179f79b',
 })
