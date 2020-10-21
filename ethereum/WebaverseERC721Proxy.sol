9186eaa97f19617a5afcc7195b2ea908555b0e6f
Avaer Kazmer <a@modules.io>
2020-10-20 21:17:57 -0400

    Update WebaverseERC721Proxy.sol

--- ethereum/WebaverseERC721Proxy.sol
+++ ethereum/WebaverseERC721Proxy.sol
@@ -17,26 +17,28 @@ contract WebaverseERC721Proxy {
     }
 
     event Withdrew(address from, uint256 tokenId, uint256 timestamp);
-    event Deposited(address to, uint256 tokenId, uint256 timestamp);
+    // event Deposited(address to, uint256 tokenId, uint256 timestamp);
     
-    function withdraw(address to, uint256 tokenId, uint256 timestamp, bytes32 r, bytes32 s, uint8 v) public {
+    function withdraw(address to, uint256 tokenId, uint256 hash, string memory filename, uint256 timestamp, bytes32 r, bytes32 s, uint8 v) public {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes memory message = abi.encodePacked(to, tokenId, timestamp, chainId);
-        bytes32 messageHash = keccak256(message);
-        bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, messageHash));
+        bytes memory message = abi.encodePacked(to, tokenId, hash, keccak256(abi.encodePacked(filename)), timestamp, chainId);
+        bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, keccak256(message)));
         address contractAddress = address(this);
         require(ecrecover(prefixedHash, v, r, s) == globalOwner, "invalid signature");
-        require(!usedWithdrawHashes[prefixedHash]);
+        require(!usedWithdrawHashes[prefixedHash], "hash already used");
         usedWithdrawHashes[prefixedHash] = true;
 
+        if (!parent.tokenExists(tokenId)) {
+            parent.mintTokenId(contractAddress, tokenId, hash, filename);
+        }
         parent.safeTransferFrom(contractAddress, to, tokenId);
         
         emit Withdrew(to, tokenId, timestamp);
     }
-    function deposit(address from, uint256 tokenId, uint256 timestamp) public {
+    /* function deposit(address from, uint256 tokenId, uint256 timestamp) public {
         address contractAddress = address(this);
         parent.safeTransferFrom(from, contractAddress, tokenId);
 
         emit Deposited(from, tokenId, timestamp);
-    }
+    } */
 }
