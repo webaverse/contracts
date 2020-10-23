5161b6d863e9eece8a876a840c69020b08c878af
Avaer Kazmer <a@modules.io>
2020-10-23 19:12:29 -0400

    Update WebaverseERC20Proxy.sol

--- ethereum/WebaverseERC20Proxy.sol
+++ ethereum/WebaverseERC20Proxy.sol
@@ -12,8 +12,9 @@ contract WebaverseERC20Proxy {
     mapping (address => uint256) deposits;
     mapping (bytes32 => bool) usedWithdrawHashes;
     
-    constructor (address parentAddress, uint256 _chainId) public {
-        signer = msg.sender;
+    // 0xfa80e7480e9c42a9241e16d6c1e7518c1b1757e4
+    constructor (address parentAddress, address signerAddress, uint256 _chainId) public {
+        signer = signerAddress;
         chainId = _chainId;
         parent = WebaverseERC20(parentAddress);
     }
@@ -25,6 +26,39 @@ contract WebaverseERC20Proxy {
         require(msg.sender == signer, "new signer can only be set by old signer");
         signer = newSigner;
     }
+    
+    /* function addr2Bytes(address a) internal pure returns (bytes memory b) {
+        assembly {
+            let m := mload(0x40)
+            a := and(a, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+            mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
+            mstore(0x40, add(m, 52))
+            b := m
+       }
+    }
+    function bytes2addr(bytes memory b) internal pure returns (address) {
+        uint result = 0;
+        for (uint i = 0; i < b.length; i++) {
+            uint c = uint256(uint8(b[i]));
+            if (c >= 48 && c <= 57) {
+                result = result * 16 + (c - 48);
+            }
+            if(c >= 65 && c<= 90) {
+                result = result * 16 + (c - 55);
+            }
+            if(c >= 97 && c<= 122) {
+                result = result * 16 + (c - 87);
+            }
+        }
+        return address(result);
+    }
+    function xorMemory(bytes memory a, bytes memory b) internal pure returns (bytes memory) {
+       bytes memory c = new bytes(a.length);
+       for (uint i = 0; i < a.length; i++) {
+           c[i] = a[i] ^ b[i];
+       }
+       return c;
+    } */
     
     // 0x08E242bB06D85073e69222aF8273af419d19E4f6, 1, 10, 0xc336b0bb5cac4584d79e77b1680ab789171ebc95f44f68bb1cc0a7b1174058ad, 0x72b888e952c0c39a8054f2b6dc41df645f5d4dc3d9cc6118535d88aa34945440, 0x1c
     function withdraw(address to, uint256 amount, uint256 timestamp, bytes32 r, bytes32 s, uint8 v) public {
