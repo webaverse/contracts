99aa69bb893d906532c8912e6926ce3f593f9cbc
Avaer Kazmer <a@modules.io>
2020-10-25 21:11:17 -0400

    Update WebaverseAccount.sol

--- ethereum/WebaverseAccount.sol
+++ ethereum/WebaverseAccount.sol
@@ -17,30 +17,4 @@ contract WebaverseAccount {
         require(msg.sender == owner);
         addressToMetadata[owner][key] = value;
     }
-    function setMetadata2(address owner, string memory key, string memory value, string memory key2, string memory value2) public {
-        require(msg.sender == owner);
-        addressToMetadata[owner][key] = value;
-        addressToMetadata[owner][key2] = value2;
-    }
-    function setMetadata3(address owner, string memory key, string memory value, string memory key2, string memory value2, string memory key3, string memory value3) public {
-        require(msg.sender == owner);
-        addressToMetadata[owner][key] = value;
-        addressToMetadata[owner][key2] = value2;
-        addressToMetadata[owner][key3] = value3;
-    }
-    function setMetadata4(address owner, string memory key, string memory value, string memory key2, string memory value2, string memory key3, string memory value3, string memory key4, string memory value4) public {
-        require(msg.sender == owner);
-        addressToMetadata[owner][key] = value;
-        addressToMetadata[owner][key2] = value2;
-        addressToMetadata[owner][key3] = value3;
-        addressToMetadata[owner][key4] = value4;
-    }
-    function setMetadata4(address owner, string memory key, string memory value, string memory key2, string memory value2, string memory key3, string memory value3, string memory key4, string memory value4, string memory key5, string memory value5) public {
-        require(msg.sender == owner);
-        addressToMetadata[owner][key] = value;
-        addressToMetadata[owner][key2] = value2;
-        addressToMetadata[owner][key3] = value3;
-        addressToMetadata[owner][key4] = value4;
-        addressToMetadata[owner][key5] = value5;
-    }
 }
