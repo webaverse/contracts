cff161387d258e4f4dddcc3f20b4222d9ff44230
Avaer Kazmer <a@modules.io>
2020-12-25 02:58:43 -0500

    Update address.js

--- ethereum/address.js
+++ ethereum/address.js
@@ -2,6 +2,7 @@ export default {
   "mainnet": {
     "Account": "0x818EA83e5747258b6Bc421C9b8C2147059f42FC8",
     "FT": "0x070cdf3359af1889bcb243666fb735e4047bc108",
+    "NFT": "0xaa11ca1df297840a6d0fccb264a78e355e449564",
     "FTProxy": "0x91b3acfb92BCEf1DE4D5069e95140Ea0C9218695"
   },
   "main": {
