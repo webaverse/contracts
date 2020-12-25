fc996c9d852e691d4e0b3efeaf2bfd8e662554c0
Avaer Kazmer <a@modules.io>
2020-12-25 01:05:51 -0500

    Add ERC20 cap argument support

--- ethereum/WebaverseERC20.sol
+++ ethereum/WebaverseERC20.sol
@@ -14,7 +14,8 @@ contract WebaverseERC20 is ERC20Capped {
      * @dev Sets the value of the `cap`. This value is immutable, it can only be
      * set once during construction.
      */
-    constructor (string memory name, string memory symbol) public ERC20(name, symbol) ERC20Capped(1e27) {
+    // 1000000000000000000000000000
+    constructor (string memory name, string memory symbol, uint256 cap) public ERC20(name, symbol) ERC20Capped(cap) {
         allowedMinters[msg.sender] = true;
         numAllowedMinters = 1;
     }
