d436338a30272267409819cf41257ee15510e67c
Avaer Kazmer <a@modules.io>
2021-01-22 01:01:11 -0500

    Bugfix chain-id.js chain names

--- config/chain-id.js
+++ config/chain-id.js
@@ -1,10 +1,20 @@
 export default {
-  "main": {
+  "mainnet": {
     "FT": 1,
     "NFT": 2,
     "LAND": 5
   },
-  "sidechain": {
+  "mainnetsidechain": {
+    "FT": 3,
+    "NFT": 4,
+    "LAND": 6
+  },
+  "rinkeby": {
+    "FT": 1,
+    "NFT": 2,
+    "LAND": 5
+  },
+  "rinkebysidechain": {
     "FT": 3,
     "NFT": 4,
     "LAND": 6
