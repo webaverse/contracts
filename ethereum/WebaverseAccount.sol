e5b656d07ce5a91e8ff501563b81e09c1ee24c02
shawticus <shawmakesmagic@gmail.com>
2021-03-22 17:36:41 -0700

    Add comments to contracts with natspec formatting

--- ethereum/WebaverseAccount.sol
+++ ethereum/WebaverseAccount.sol
@@ -2,27 +2,45 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
+/** @title Webaverse account contract. */
 contract WebaverseAccount {
-    event MetadataSet(
-      address owner,
-      string key,
-      string value
-    );
+    /** @dev Event called when owner changes metadata value. */
+    event MetadataSet(address owner, string key, string value);
 
-    mapping (address => mapping(string => string)) private addressToMetadata;
+    /** @dev Mapping of a key like "avatar" to a token address. */
+    mapping(address => mapping(string => string)) private addressToMetadata;
 
+    /** @dev Constructor is empty. */
     constructor() public {}
 
-    // 0x08E242bB06D85073e69222aF8273af419d19E4f6, "lol"
-    function getMetadata(address owner, string memory key) public view returns (string memory) {
+    /**  @dev Set the metadata value for a key
+     *  Example: 0x08E242bB06D85073e69222aF8273af419d19E4f6, "avatar"
+     *  @param owner Owner of this account
+     *  @param key Metadata key to retrieve
+     *  @return value as a string
+     */
+    function getMetadata(address owner, string memory key)
+        public
+        view
+        returns (string memory)
+    {
         return addressToMetadata[owner][key];
     }
-    
-    // 0x08E242bB06D85073e69222aF8273af419d19E4f6, "lol", "zol"
-    function setMetadata(address owner, string memory key, string memory value) public {
-        require(msg.sender == owner);
-        addressToMetadata[owner][key] = value;
-        
-        emit MetadataSet(owner, key, value);
+
+    /**  @dev Set the metadata value for a key
+     *  Example: 0x08E242bB06D85073e69222aF8273af419d19E4f6, "avatar", "<address_of_avatar_token>"
+     *  @param owner Owner of this account
+     *  @param key Metadata key to store
+     *  @param value Metadata value to store
+     */
+    function setMetadata(
+        address owner,
+        string memory key,
+        string memory value
+    ) public {
+        require(msg.sender == owner); // only allow owner to change metadata
+        addressToMetadata[owner][key] = value; // store the value in the key-value mapping
+
+        emit MetadataSet(owner, key, value); // emit the MetadataSet event
     }
 }
