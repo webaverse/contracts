05da0afbdd3deeaa78ff0d724f66a9eafef2482b
shawticus <shawmakesmagic@gmail.com>
2021-03-23 11:03:15 -0700

    Add rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+require ('dotenv').config()
 
 module.exports = {
   contracts_directory: "./ethereum",
@@ -10,15 +9,22 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 4
+      // gas: 2700000,
+      // gasPrice: 10000000000
+    },
     matic: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mainnet.matic.network`),
+      provider: () => new HDWalletProvider(process.env.matic, `https://rpc-mainnet.matic.network`),
       network_id: 137,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: false
     },
     maticTestnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      provider: () => new HDWalletProvider(process.env.maticTestnet, `https://rpc-mumbai.matic.today`),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 200,
