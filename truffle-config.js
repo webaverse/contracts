3907390f75316b0522879641938f044cd583952d
soulofmischief <hello@bad-software.com>
2021-03-24 03:37:03 -0500

    Fix broken reference to environment var

--- truffle-config.js
+++ truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
     rinkeby:{
       host: "localhost",
       provider: function() {
-      return new HDWalletProvider( mnemonic, "https://rinkeby.infura.io/v3/" + INFURA_API_KEY);
+      return new HDWalletProvider( process.env.rinkeby, "https://rinkeby.infura.io/v3/" + INFURA_API_KEY);
       },
       network_id:4, 
       gas : 6700000,
