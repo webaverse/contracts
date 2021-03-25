0967e1026191096b0de79a7c773ddb6cfe49f6b6
shawticus <shawmakesmagic@gmail.com>
2021-03-25 03:24:55 -0700

    Add to config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-require ('dotenv').config()
+require('dotenv').config()
 
 module.exports = {
   contracts_directory: "./ethereum",
@@ -9,13 +9,29 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    rinkeby:{
+    rinkeby: {
       host: "localhost",
       provider: () => new HDWalletProvider(process.env.rinkeby, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
-      network_id:4, 
-      gas : 6700000,
-      gasPrice : 10000000000
-      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000
+    },
+    webaverse: {
+      host: "127.0.0.1",
+      // provider: () => new HDWalletProvider(process.env.webaverse, "http://ethereum.exokit.org"),
+      port: 1337,
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000
+    },
+    webaverseTestnet: {
+      host: "127.0.0.1",
+      // provider: () => new HDWalletProvider(process.env.webaverse, "http://ethereum.exokit.org"),
+      port: 1338,
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000
+    },
     polygon: {
       provider: () => new HDWalletProvider(process.env.polygon, `https://rpc-mainnet.polygon.network`),
       network_id: 137,
@@ -40,13 +56,13 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-        version: "^0.6.2",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 1500
-          }
+      version: "^0.6.2",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1500
         }
+      }
     }
   }
 }
